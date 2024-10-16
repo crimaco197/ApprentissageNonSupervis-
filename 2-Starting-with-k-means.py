3e63982ae61a68f634756374aa0df97a21eb24e8
EmilieGreaker1 <emigre01@hotmail.com>
2024-10-16 16:39:38 +0200

    TP 1

--- 2-Starting-with-k-means.py
+++ 2-Starting-with-k-means.py
@@ -13,7 +13,7 @@ from sklearn import metrics
 # Exemple :  k-Means Clustering
 
 path = './artificial/'
-name="R15.arff"
+name="birch-rg3.arff"
 
 #path_out = './fig/'
 databrut = arff.loadarff(open(path+str(name), 'r'))
@@ -29,19 +29,29 @@ plt.scatter(f0, f1, s=8)
 plt.title("Donnees initiales : "+ str(name))
 plt.show()
 
-# Initialize an empty list to store inertia values
+# Initialize an empty list to store inertia values, silhouette scores and runtimes for k-means and mini-batch
 inertias = []
+silhouette_scores = []
+runtimes = []
+runtimesMini = []
 
 # Loop over the different values of k
-for k in range(1, 20):
+for k in range(1, 50):
     print("------------------------------------------------------")
-    print("Appel KMeans pour une valeur de k fixée")
+    print("Appel KMeans pour une valeur de k fixée à ", k)
     tps1 = time.time()
     
     # Run KMeans with the current k value
     model = cluster.KMeans(n_clusters=k, init='k-means++', n_init=1)
     model.fit(datanp)
     tps2 = time.time()
+
+    tps_minibatch_1 = time.time()
+
+    # Run Mini-Batch with the current k value
+    modelMini = cluster.MiniBatchKMeans(n_clusters=k, init='k-means++', n_init=1)
+    modelMini.fit(datanp)
+    tps_minibatch_2 = time.time()
     
     labels = model.labels_
     iteration = model.n_iter_
@@ -51,19 +61,46 @@ for k in range(1, 20):
     # Append the inertia to the list
     inertias.append(inertie)
 
+    # Calculate the silhouette score and append it
+    if k > 1:
+        silhouette_scores.append(metrics.silhouette_score(datanp, labels))
+
     # Scatter plot of the clustering result
-    plt.scatter(f0, f1, c=labels, s=8)
-    plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=50, linewidths=3, color="red")
-    plt.title("Données après clustering : " + str(name) + " - Nb clusters =" + str(k))
-    plt.show()
+    #plt.scatter(f0, f1, c=labels, s=8)
+    #plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=50, linewidths=3, color="red")
+    #plt.title("Données après clustering : " + str(name) + " - Nb clusters =" + str(k))
+    #plt.show()
+
+    # Append the k-means runtime and mini-batch runtime to their respective lists
+    runtimes.append(round((tps2 - tps1) * 1000, 2))
+    runtimesMini.append(round((tps_minibatch_2 - tps_minibatch_1) * 1000, 2))
 
     print("nb clusters =", k, ", nb iter =", iteration, ", inertie = ", inertie, ", runtime = ", round((tps2 - tps1) * 1000, 2), "ms")
 
-# Plot the relation between number of clusters and inertia
+# Plot the relation between number of clusters and runtime
 plt.figure(figsize=(8, 6))
-plt.plot(range(1, 20), inertias, marker='o')
-plt.title("Relation entre le nombre de clusters et l'inertie")
+plt.plot(range(2, 50), runtimes[1:50], marker='o')
+plt.plot(range(2, 50), runtimesMini[1:50], marker='o')
+plt.title("Relation entre le nombre de clusters et le temps de calcul")
 plt.xlabel("Nombre de clusters")
-plt.ylabel("Inertie")
+plt.ylabel("Temps de calcul")
 plt.grid(True)
 plt.show()
+
+# Plot the relation between number of clusters and inertia
+#plt.figure(figsize=(8, 6))
+#plt.plot(range(10, 25), inertias[9:25], marker='o')
+#plt.title("Relation entre le nombre de clusters et l'inertie")
+#plt.xlabel("Nombre de clusters")
+#plt.ylabel("Inertie")
+#plt.grid(True)
+#plt.show()
+
+# Plot the relation between number of clusters and silhouette scores
+#plt.figure(figsize=(8, 6))
+#plt.plot(range(2, 25), silhouette_scores, marker='o')
+#plt.title("Relation entre le nombre de clusters et le coefficient de silhouette")
+#plt.xlabel("Nombre de clusters")
+#plt.ylabel("Coefficient de silhouette")
+#plt.grid(True)
+#plt.show()
